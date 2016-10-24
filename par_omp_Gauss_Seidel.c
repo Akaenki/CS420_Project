fe80baf4c2eb3fab6b640ff8dfd3ee6adab98793
Gao <ggao5@taubh2.campuscluster.illinois.edu>
2016-10-24 17:47:47 -0500

    accuracy of basic_gauss_seidel.c is verified with the red-black algorithm

--- par_omp_Gauss_Seidel.c
+++ par_omp_Gauss_Seidel.c
@@ -20,12 +20,11 @@ void par_omp_gauss_seidel()
   for (iter = 0; iter < ITERS ; iter++)
   {
     for(d=0;d<N+N-5;d++){
-      ifirst=(d<N-1)?d+1:N-2;
-      ilast=(d<N-1)?1:d-N+3;
-      #pragma omp parallel for private(j)
-      for (i = ifirst; i > ilast; i--)
+      ifirst=(d<N-2)?d+1:N-2;
+      ilast=(d<N-2)?1:d-N+4;
+      for (i = ifirst; i >= ilast; i--)
       {
-          j=d+2-i;
+          int j=d+2-i;
           array[i][j]=0.25*(array[i-1][j]+array[i+1][j]+array[i][j-1]+array[i][j+1]);
       }
       }
