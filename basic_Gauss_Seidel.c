8d94f5c0b7dd6c86a6c880802fec8a32ed0bf36f
Linling Miao <Linling@lmiao4.scs.illinois.edu>
2016-11-04 17:05:52 -0500

    Update the basic algorithm

--- basic_Gauss_Seidel.c
+++ basic_Gauss_Seidel.c
@@ -4,29 +4,27 @@ extern double array[N][N];
  * Implements the basic sequential Red-Black Jacobi Relaxation.
  * Use this as a stub for your other implementations.
  */
-void basic_gauss_seidel()
-{
-  int i, j, d;
-  int iter;
-  int ifirst, ilast;
-  for(j=0;j<N;j++)//setting the boundary condition
-    array[0][j]=BOTTOM_BOUNDARY_VALUE;
-  for(j=0;j<N;j++)
-    array[N-1][j]=TOP_BOUNDARY_VALUE;
-  for(i=1;i<N-1;i++)
-    array[i][0]=LEFT_BOUNDARY_VALUE;
-  for(i=1;i<N-1;i++)
-    array[i][N-1]=RIGHT_BOUNDARY_VALUE;
-  for (iter = 0; iter < ITERS ; iter++)//loop through ITERS times
-  {
-    for(d=0;d<N+N-5;d++){//loop through each diagnoal/sub diagonal line
-      ifirst=(d<N-2)?d+1:N-2;
-      ilast=(d<N-2)?1:d-N+4;
-      for (i = ifirst; i >= ilast; i--)//loop through each entries in the diagonal line
-      {
-          j=d+2-i;
-          array[i][j]=0.25*(array[i-1][j]+array[i+1][j]+array[i][j-1]+array[i][j+1]);//Gauss-Seidel
-      }
-      }
-  }
+void basic_gauss_seidel(){
+    for(int iter = 0; iter < ITERS; iter++){
+        for(int d = 0; d < N+N-1; d++){ //total # of diagonals: 2N-2
+            int ifirst = (d < N) ? d : N-1;
+            int ilast  = (d < N) ? 0 : d-N+1;
+            for(int i = ifirst; i >= ilast; i--){ //from left to right
+                int j = d - i;
+                
+                double top, bottom, left, right;
+                
+                if(i == 0) top = TOP_BOUNDARY_VALUE;
+                else top  = array[i-1][j];
+                if(i == N-1) bottom = BOTTOM_BOUNDARY_VALUE;
+                else bottom = array[i+1][j];
+                if(j = 0) left = LEFT_BOUNDARY_VALUE;
+                else left = array[i][j-1];
+                if(j = N-1) right = RIGHT_BOUNDARY_VALUE;
+                else right = array[i][j+1];
+                
+                array[i][j] = 0.25*(top + bottom + left + right);
+            }
+        }
+    }
 }
--- par_omp_Gauss_Seidel.c
+++ par_omp_Gauss_Seidel.c
@@ -1,42 +1,54 @@
 extern double array[N][N];
 #include <omp.h>
+#include <stdio.h>
 
 /**
  * Implements the basic sequential Red-Black Jacobi Relaxation.
  * Use this as a stub for your other implementations.
  */
-void par_omp_gauss_seidel()
-{
-  int i, j, d;
-  int iter;
-  int ifirst, ilast;
-  for(j=0;j<N;j++)
-    array[0][j]=BOTTOM_BOUNDARY_VALUE;
-  for(j=0;j<N;j++)
-    array[N-1][j]=TOP_BOUNDARY_VALUE;
-  for(i=1;i<N-1;i++)
-    array[i][0]=LEFT_BOUNDARY_VALUE;
-  for(i=1;i<N-1;i++)
-    array[i][N-1]=RIGHT_BOUNDARY_VALUE;
-  for (iter = 0; iter < ITERS ; iter++)
-  {
-    for(d=0;d<N+N-5;d++){
-      ifirst=(d<N-2)?d+1:N-2;
-      ilast=(d<N-2)?1:d-N+4;
-      if (ifirst-ilast>BEGIN_LENGTH){//decide whether or not use openmpi
-      #pragma omp parallel for
-      for (i = ifirst; i >= ilast; i--)
-      {
-          int j=d+2-i;
-          array[i][j]=0.25*(array[i-1][j]+array[i+1][j]+array[i][j-1]+array[i][j+1]);
-      }
-      }
-      else
-      for (i = ifirst; i >= ilast; i--)
-      {
-          int j=d+2-i;
-          array[i][j]=0.25*(array[i-1][j]+array[i+1][j]+array[i][j-1]+array[i][j+1]);
-      }
-      }
-  }
+void par_omp_gauss_seidel(){
+    for(int iter = 0; iter < ITERS; iter++){
+        for(int d = 0; d < N+N-1; d++){ //total # of diagonals: 2N-2
+            int ifirst = (d < N) ? d : N-1;
+            int ilast  = (d < N) ? 0 : d-N+1;
+            
+            if((ifirst-ilast) >= BEGIN_LENGTH){ //parallel
+#pragma omp parallel for schedule(dynamic)
+                for(int i = ifirst; i >= ilast; i--){ //from left to right
+                    int j = d - i;
+                    
+                    double top, bottom, left, right;
+                    
+                    if(i == 0) top = TOP_BOUNDARY_VALUE;
+                    else top  = array[i-1][j];
+                    if(i == N-1) bottom = BOTTOM_BOUNDARY_VALUE;
+                    else bottom = array[i+1][j];
+                    if(j = 0) left = LEFT_BOUNDARY_VALUE;
+                    else left = array[i][j-1];
+                    if(j = N-1) right = RIGHT_BOUNDARY_VALUE;
+                    else right = array[i][j+1];
+                    
+                    array[i][j] = 0.25*(top + bottom + left + right);
+                }
+            } else{ //do not parallel
+                for(int i = ifirst; i >= ilast; i--){
+                    int j = d - i;
+                    
+                    double top, bottom, left, right;
+                    
+                    if(i == 0) top = TOP_BOUNDARY_VALUE;
+                    else top  = array[i-1][j];
+                    if(i == N-1) bottom = BOTTOM_BOUNDARY_VALUE;
+                    else bottom = array[i+1][j];
+                    if(j = 0) left = LEFT_BOUNDARY_VALUE;
+                    else left = array[i][j-1];
+                    if(j = N-1) right = RIGHT_BOUNDARY_VALUE;
+                    else right = array[i][j+1];
+                    
+                    array[i][j] = 0.25*(top + bottom + left + right);
+                }
+            }
+        }
+    }
 }
+
